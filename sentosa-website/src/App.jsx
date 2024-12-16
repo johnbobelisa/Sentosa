24f33c1ca6ea59f8b7e4ce0b074cca7aecfa9620
johnbobelisa <johnelisaaa@gmail.com>
2024-12-17 00:03:09 +1100

    fix vite config

--- sentosa-website/src/App.jsx
+++ sentosa-website/src/App.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import HomeContainer from "./components/HomeContainer";
-import "./App.css"; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./pages/home"
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Sentosa">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
@@ -15,4 +11,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
